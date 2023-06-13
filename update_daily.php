bd5ed46d8cf40cb21f948acc3bff6b8e0716862e
redwhiteandblue7 <tprosser@protonmail.com>
2023-06-13 22:33:13 +0100

    Removed some unneeded files

--- update_daily.php
+++
@@ -1,5 +0,0 @@
-<?php
-	define('INCLUDE_PATH', 'engine/');
-	require_once(INCLUDE_PATH . 'update_daily.php');
-	exit();
-?>
--- update_stats.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('INCLUDE_PATH', 'engine/');
-	$dn = "moviesample.net";
-	require_once(INCLUDE_PATH . 'update_stats.php');
-	exit();
-?>
